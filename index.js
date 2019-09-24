eb5420063ee4f1457630741a337dba7397066068
Oliver <olivercern@gmail.com>
2019-09-24 09:47:12 +0200

    part3/phonebook/step8

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const app = express();
 app.use(bodyParser.json());
-app.use(morgan("tiny"));
+morgan.token("body", (request, response) => {
+    return JSON.stringify(request.body);
+});
+app.use(
+    morgan(
+        ":method :url :status :res[content-length] - :response-time ms :body"
+    )
+);
 
 let persons = [
     {
