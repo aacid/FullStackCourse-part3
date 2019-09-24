908ae396f84ff5885bfd0ba577c90a86b4cc2b59
Oliver <olivercern@gmail.com>
2019-09-24 10:30:00 +0200

    uibuild

---
+++ build/precache-manifest.561881a2348c71bebb65ebd1d882cb04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16dba0e165e13b6f255cfdbddaf115e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09be5a770350be74414d",
+    "url": "/static/css/main.4c5831e4.chunk.css"
+  },
+  {
+    "revision": "6b63a8a3ef27e8cebaba",
+    "url": "/static/js/2.d9789448.chunk.js"
+  },
+  {
+    "revision": "09be5a770350be74414d",
+    "url": "/static/js/main.a697fe7a.chunk.js"
+  },
+  {
+    "revision": "643b1496a8e20922303c",
+    "url": "/static/js/runtime~main.bf59cc56.js"
+  }
+]);
